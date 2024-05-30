cdef2ab0f55a4e6aa88a573aa0a280be0d14d795
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-05-30 13:40:00 +0200

    fix: Sort key mappings to prioritize ones with more modifiers

--- kuma-api/common/src/main/java/net/blay09/mods/kuma/ManagedKeyMappingRegistry.java
+++ kuma-api/common/src/main/java/net/blay09/mods/kuma/ManagedKeyMappingRegistry.java
@@ -3,16 +3,21 @@ package net.blay09.mods.kuma;
 import net.blay09.mods.kuma.api.ManagedKeyMapping;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ManagedKeyMappingRegistry {
+    public static final Comparator<ManagedKeyMapping> COMPARATOR = Comparator.comparingInt((ManagedKeyMapping it) -> it.getBinding().modifiers().size())
+            .reversed();
+
     private static final List<ManagedKeyMapping> keyMappings = new ArrayList<>();
 
     public static void register(ManagedKeyMapping keyMapping) {
         keyMappings.add(keyMapping);
+        keyMappings.sort(COMPARATOR);
     }
 
     public static List<ManagedKeyMapping> getKeyMappings() {
-        return keyMappings;
+        return keyMappings.stream().sorted(COMPARATOR).toList();
     }
 }
