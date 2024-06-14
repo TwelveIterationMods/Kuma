912856844629aa366e0c51911071ce7a042e39bf
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-06-14 09:33:48 +0200

    fix: Fix thread safety in ManagedKeyMappingRegistry

--- kuma-api/common/src/main/java/net/blay09/mods/kuma/ManagedKeyMappingRegistry.java
+++ kuma-api/common/src/main/java/net/blay09/mods/kuma/ManagedKeyMappingRegistry.java
@@ -3,6 +3,7 @@ package net.blay09.mods.kuma;
 import net.blay09.mods.kuma.api.ManagedKeyMapping;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -10,7 +11,7 @@ public class ManagedKeyMappingRegistry {
     public static final Comparator<ManagedKeyMapping> COMPARATOR = Comparator.comparingInt((ManagedKeyMapping it) -> it.getBinding().modifiers().size())
             .reversed();
 
-    private static final List<ManagedKeyMapping> keyMappings = new ArrayList<>();
+    private static final List<ManagedKeyMapping> keyMappings = Collections.synchronizedList(new ArrayList<>());
 
     public static void register(ManagedKeyMapping keyMapping) {
         keyMappings.add(keyMapping);
@@ -18,6 +19,8 @@ public class ManagedKeyMappingRegistry {
     }
 
     public static List<ManagedKeyMapping> getKeyMappings() {
-        return keyMappings.stream().sorted(COMPARATOR).toList();
+        synchronized (keyMappings) {
+            return keyMappings.stream().sorted(COMPARATOR).toList();
+        }
     }
 }
