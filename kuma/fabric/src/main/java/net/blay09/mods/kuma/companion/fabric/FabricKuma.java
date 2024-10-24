48c43e1fd9304e3d0cf68712015d947a97fdfa3b
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-10-24 18:44:46 +0200

    refactor: Move the Kuma companion mod out of this repo to simplify builds and since the API should change so seldomly that no benefits are gained from a monorepo

--- kuma/fabric/src/main/java/net/blay09/mods/kuma/companion/fabric/FabricKuma.java
+++
@@ -1,9 +0,0 @@
-package net.blay09.mods.kuma.companion.fabric;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class FabricKuma implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-    }
-}
