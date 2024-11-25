bbc5acb5d49b5aaba9ecb0108c2652ce268a461e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-11-25 18:41:23 +0100

    feat: Update to Forge 1.21.3

--- forge/src/main/java/net/blay09/mods/kuma/forge/ForgeKumaAPI.java
+++ forge/src/main/java/net/blay09/mods/kuma/forge/ForgeKumaAPI.java
@@ -1,15 +1,16 @@
 package net.blay09.mods.kuma.forge;
 
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(value = "kuma_api")
 public class ForgeKumaAPI {
 
-    public ForgeKumaAPI() {
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> {
-            ForgeKumaAPIClient.init();
-        });
+    public ForgeKumaAPI(IEventBus modEventBus) {
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            ForgeKumaAPIClient.init(modEventBus);
+        }
     }
 }
