7b1c75f58fc9d12f0007568cd520facbeb253eb6
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-23 10:47:59 +0100

    Revert "fix: Fix Forge's getKey() patch conflicting with Kuma https://github.com/TwelveIterationMods/Balm/issues/138"
    This reverts commit 28aad91a7b923e76d6fa9ec4bc7d6211fc33fcf5.

--- common/src/main/java/net/blay09/mods/kuma/mixin/KeyMappingAccessor.java
+++ common/src/main/java/net/blay09/mods/kuma/mixin/KeyMappingAccessor.java
@@ -7,6 +7,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(KeyMapping.class)
 public interface KeyMappingAccessor {
-    @Accessor("key")
-    InputConstants.Key kuma_getKey();
+    @Accessor
+    InputConstants.Key getKey();
 }
