e57f846bcc50bf8acc66122adfde382d6af9ed0f
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-09-08 09:39:02 +0200

    fix: Never match key mapping if it's unbound https://github.com/TwelveIterationMods/ClientTweaks/issues/106

--- common/src/main/java/net/blay09/mods/kuma/AbstractManagedKeyMapping.java
+++ common/src/main/java/net/blay09/mods/kuma/AbstractManagedKeyMapping.java
@@ -45,7 +45,7 @@ public abstract class AbstractManagedKeyMapping implements ManagedKeyMapping {
 
     @Override
     public boolean isDown() {
-        return Kuma.isDown(getKey());
+        return isBound() && Kuma.isDown(getKey());
     }
 
     @Override
@@ -55,12 +55,18 @@ public abstract class AbstractManagedKeyMapping implements ManagedKeyMapping {
 
     @Override
     public boolean matchesMouse(int button) {
+        if (isUnbound()) {
+            return false;
+        }
         final var key = getKey();
         return key.getType().equals(InputConstants.Type.MOUSE) && key.getValue() == button;
     }
 
     @Override
     public boolean matchesKey(int key, int scanCode, int modifiers) {
+        if (isUnbound()) {
+            return false;
+        }
         final var keyBinding = getKey();
         return keyBinding.getType().equals(InputConstants.Type.KEYSYM) && keyBinding.getValue() == key;
     }
