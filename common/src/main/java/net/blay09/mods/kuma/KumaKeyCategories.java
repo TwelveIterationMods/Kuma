8e463135d81d7026f5d96fb515a68ba75904b35b
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-10-28 14:16:07 +0100

    fix: Fix CME in KumaKeyCategories https://github.com/TwelveIterationMods/ClientTweaks/issues/110#issuecomment-3456358417

--- common/src/main/java/net/blay09/mods/kuma/KumaKeyCategories.java
+++ common/src/main/java/net/blay09/mods/kuma/KumaKeyCategories.java
@@ -3,12 +3,12 @@ package net.blay09.mods.kuma;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.resources.ResourceLocation;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class KumaKeyCategories {
 
-    private static final Map<String, KeyMapping.Category> defaultCategories = new HashMap<>();
+    private static final Map<String, KeyMapping.Category> defaultCategories = new ConcurrentHashMap<>();
 
     public static KeyMapping.Category getDefaultCategory(String namespace) {
         return defaultCategories.computeIfAbsent(namespace,
