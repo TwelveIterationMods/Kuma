7acf4597bacd34c7c909e51c3574d68c3403c1d7
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-19 23:29:45 +0100

    feat: Update to NeoForge 1.21.11

--- neoforge/src/main/java/net/blay09/mods/kuma/neoforge/NeoForgeManagedKeyMappingBuilder.java
+++ neoforge/src/main/java/net/blay09/mods/kuma/neoforge/NeoForgeManagedKeyMappingBuilder.java
@@ -23,7 +23,7 @@ public class NeoForgeManagedKeyMappingBuilder extends AbstractManagedKeyMappingB
 
     @Override
     protected VanillaManagedKeyMapping createVanillaKeyMapping(String name, InputBinding binding) {
-        return new VanillaManagedKeyMapping(context, screenInputHandler, worldInputHandler, keyRepeat, () -> {
+        return new VanillaManagedKeyMapping(context, screenInputHandler, worldInputHandler, keyRepeat, ignoresScreenFocus, () -> {
             final var defaultKey = binding.key();
             final var effectiveContext = mapConflictContext(context);
             final var effectiveModifier = NeoForgeKeyModifiers.toNeoForge(binding.modifiers());
